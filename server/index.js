91518895e1b92e9a7f13830fcc6cad32264aceec
Akash Singh <akashsingh914208@gmail.com>
2025-08-08 17:20:18 +0530

    change url

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ const userSessions = new Map();
 
 // GitHub OAuth endpoints
 app.get('/auth/github', (req, res) => {
-  const redirectUri = `https://tet-case-generator.onrender.com/auth/github/callback`;
+  const redirectUri = `https://tet-case-generator-2sji.vercel.app/auth/github/callback`;
   const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=${redirectUri}&scope=repo`;
   res.json({ authUrl: githubAuthUrl });
 });
